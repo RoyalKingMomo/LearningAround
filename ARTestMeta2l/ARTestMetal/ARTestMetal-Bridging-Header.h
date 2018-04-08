b12d58a390131e4025396aba4f831137661c5ed7
RoyalKingMomo <malahdal@ualberta.ca>
2018-04-07 23:28:07 -0600

    fixing git issues pt 1

---
+++ ARTestMeta2l/ARTestMetal/ARTestMetal-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ARTestMetal-Bridging-Header.h
+//  ARTestMetal
+//
+//  Created by Mohammad Al-Ahdal on 2018-04-07.
+//  Copyright © 2018 Mohammad Al-Ahdal. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
